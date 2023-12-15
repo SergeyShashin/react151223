47c5c1ab826efe432a912c08ff1b861075bde540
SergeyShashin <s9997910@yandex.ru>
2023-12-15 16:06:39 +0300

    React 151223 lesson1 done.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-import { User } from './user.js';
+// import { User } from './user.js';
 import React from 'react';
 import ReactDom from 'react-dom';
 
-console.log('Hi react!');
+// console.log('Hi react!');
 
-console.log(new User('Luke', 'SkyWalker').sayHi());
+// console.log(new User('Luke', 'SkyWalker').sayHi());
 
-let element = React.createElement(
-  'h1',
-  {className: 'react-hi'},
-  'Привет, React работает!'
-);
+// let element = React.createElement(
+//   'h1',
+//   {className: 'react-hi'},
+//   'Привет, React работает!'
+// );
+
+// let element = <h1 className='react-hi'>Привет, React JSX работает!</h1>
+
+let messages = ['Привет, друг!', 'Как поживаешь?'];
+
+let Message = props => <h2>{props.message}</h2>;
+let MessageList = props => props.messages.map(el => <Message message={el} />);
 
 ReactDom.render(
-  element,
+  <MessageList messages={messages} />,
   document.getElementById('root')
 );
