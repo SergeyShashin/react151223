f52b46bb9c950b7ea4f41350d9201341daf1b9e2
SergeyShashin <s9997910@yandex.ru>
2023-12-18 11:16:05 +0300

    React 151223 lesson2 handleClick done.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,26 @@ import React, { Component } from 'react';
 export class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      counter: 0
+    }
+  }
+
+  handleClick = event => {
+    let sign = event.target.dataset.sign;
+    console.log(sign);
+    this.setState(prevState => prevState.counter += sign === '+' ? 1 : -1);
   }
 
   render() {
 
     return (
-      <div>Компонент App</div>
+      <div>
+        <button data-sign='+' onClick={this.handleClick}>+</button>
+        <span>{this.state.counter}</span>
+        <button data-sign='-' onClick={this.handleClick}>-</button>
+      </div>
     );
 
   }
