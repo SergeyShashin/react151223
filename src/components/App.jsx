5631b7211c2032ca27cc083707b0a0d37c61a43e
SergeyShashin <s9997910@yandex.ru>
2023-12-18 10:40:03 +0300

    React 151223 lesson2 start.

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    return (
+      <div>Компонент App</div>
+    );
+
+  }
+
+}
---
+++ src/index.jsx
@@ -0,0 +1,30 @@
+// import { User } from './user.js';
+import React from 'react';
+// import ReactDom from 'react-dom';
+import ReactDom from 'react-dom/client';
+import { App } from 'components/App';
+
+// console.log('Hi react!');
+
+// console.log(new User('Luke', 'SkyWalker').sayHi());
+
+// let element = React.createElement(
+//   'h1',
+//   {className: 'react-hi'},
+//   'Привет, React работает!'
+// );
+
+// let element = <h1 className='react-hi'>Привет, React JSX работает!</h1>;
+
+// let messages = ['Привет, друг!', 'Как поживаешь?'];
+
+// let Message = props => <h2>{props.message}</h2>;
+// let MessageList = props => props.messages.map(el => <Message message={el} />);
+
+// ReactDom.render(
+// <MessageList messages={messages} />,
+//   <App />,
+//   document.getElementById('root')
+// );
+
+ReactDom.createRoot(document.getElementById('root')).render(<App />);
